291a3afdd6b92b0687aaab8fed346db6773d1666
jfgarcia0911 <jfgarcia0911@gmail.com>
2025-08-05 02:41:40 +0800

    added Footer Components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   useEffect(() => {
@@ -20,6 +21,7 @@ function App() {
       <Skills/>
       <Projects/>
       <Contact/>
+      <Footer />
 		</main>
     
   );
