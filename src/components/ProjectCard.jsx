403284ee4c0185758933afa2b42e7b2a750e765e
jfgarcia0911 <jfgarcia0911@gmail.com>
2025-09-30 01:11:14 +0800

    feat: add project details in Projects component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,50 +1,64 @@
 import { useState } from "react";
 
-export default function ProjectCard({ image, title, description, link }) {
-    const [expanded, setExpanded] = useState(false);
+export default function ProjectCard({ image, title, description, link, code }) {
+	const [expanded, setExpanded] = useState(false);
 
-    return (
-        <article className="relative max-w-sm bg-gray-800 rounded overflow-hidden shadow-lg group flex flex-col self-start w-100">
-            <div className="absolute z-0 w-40 h-40 sm:w-60 sm:h-60 bg-[#483cf5] rounded-full blur-3xl opacity-50 -top-5 left-10"></div>
+	return (
+		<article className="relative max-w-sm bg-gray-800 rounded overflow-hidden shadow-lg group flex flex-col self-start w-100">
+			<div className="absolute z-0 w-40 h-40 sm:w-60 sm:h-60 bg-[#483cf5] rounded-full blur-3xl opacity-50 -top-5 left-10"></div>
 
-            <div className="relative z-10 flex flex-col h-full">
-                <figure className="relative">
-                    <img
-                        src={image}
-                        alt={title}
-                        className="w-full h-48 object-contain transition-transform duration-300 group-hover:scale-110"
-                    />
-                    <a
-                        href={link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="absolute inset-0 h-[202px] flex items-center justify-center bg-blue-800 bg-opacity-50 opacity-0 group-hover:opacity-80 transition-opacity duration-300"
-                    >
-                        <button className="bg-white font-medium text-black py-2 px-4 rounded-3xl shadow hover:text-white hover:bg-[#2879d5] cursor-pointer">
-                            Live Preview
-                        </button>
-                    </a>
-                </figure>
+			<div className="relative z-10 flex flex-col h-full">
+				<figure className="relative">
+					<img
+						src={image}
+						alt={title}
+						className="w-full h-48 object-contain transition-transform duration-300 group-hover:scale-110"
+					/>
+					<div className="absolute gap-3 inset-0 h-[202px] flex items-center justify-center bg-blue-800 bg-opacity-50 opacity-0 group-hover:opacity-80 transition-opacity duration-300">
+						<a
+							href={link}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<button className="bg-white font-medium text-black py-2 px-4 rounded-3xl shadow hover:text-white hover:bg-[#2879d5] cursor-pointer">
+								Live Preview
+							</button>
+						</a>
+						<a
+							href={code}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<button className="bg-white font-medium text-black py-2 px-4 rounded-3xl shadow hover:text-white hover:bg-[#2879d5] cursor-pointer">
+								View Code
+							</button>
+						</a>
+					</div>
+				</figure>
 
-                <div className="px-6 py-4 flex flex-col flex-grow">
-                    <header>
-                        <h3 className="text-white font-bold text-xl mb-2">{title}</h3>
-                    </header>
+				<div className="px-6 py-4 flex flex-col flex-grow">
+					<header>
+						<h3 className="text-white font-bold text-xl mb-2">
+							{title}
+						</h3>
+					</header>
 
-                    <p
-                        className={`text-gray-200 ${expanded ? "" : "line-clamp-2"} `}
-                    >
-                        {description}
-                    </p>
+					<p
+						className={`text-gray-200 ${
+							expanded ? "" : "line-clamp-2"
+						} `}
+					>
+						{description}
+					</p>
 
-                    <span
-                        onClick={() => setExpanded(!expanded)}
-                        className="text-blue-500 cursor-pointer mt-1"
-                    >
-                        {expanded ? "See less" : "See more"}
-                    </span>
-                </div>
-            </div>
-        </article>
-    );
+					<span
+						onClick={() => setExpanded(!expanded)}
+						className="text-blue-500 cursor-pointer mt-1"
+					>
+						{expanded ? "See less" : "See more"}
+					</span>
+				</div>
+			</div>
+		</article>
+	);
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,78 +1,95 @@
-import React from 'react'
-import amazon from '/amazon.png'
-import portfolio from '/webportfolio2023.png'
-import portfolio2 from '/portfolio2.png'
-import authDemo from '/auth-demo.png'
+import React from "react";
+import amazon from "/amazon.png";
+import portfolio from "/webportfolio2023.png";
+import portfolio2 from "/portfolio2.png";
+import authDemo from "/auth-demo.png";
+import jobseeker from "/jobseeker.png";
+import fbclone from "/fbclone.png";
 
-import ProjectCard from './ProjectCard.jsx'
-
+import ProjectCard from "./ProjectCard.jsx";
 
 export default function Projects() {
+	const listProjects = [
+		{
+			image: amazon,
+			title: "Amazon Clone App",
+			description:
+				"A full-stack e-commerce web application inspired by Amazon. Features include product listings, category-based search, detailed product pages, authentication, cart management, and order tracking. Built with React, Next.js, Firebase, and Tailwind CSS, the app delivers a responsive and user-friendly shopping experience.",
+			link: "https://amazon-clone-peach-five-34.vercel.app/",
+			code: "https://github.com/jfgarcia0911/amazon-clone",
+		},
+		{
+			image: fbclone,
+			title: "FB Clone",
+			description:
+				"A full-stack social media application inspired by Facebook, built with modern web technologies to replicate core social networking features including story sharing, image posts, and real-time interactions.",
+			link: "https://github.com/jfgarcia0911/fb-clone",
+			code: "https://github.com/jfgarcia0911/fb-clone",
+		},
+		{
+			image: portfolio2,
+			title: "Web Portfolio 2025",
+			description:
+				"A modern, responsive portfolio website built with React 19 and Vite, featuring sleek animations and optimized performance. This portfolio showcases my work and skills with a clean, professional design.",
+			link: "https://jfgarcia0911.github.io/jfportfolio-2/",
+			code: "https://github.com/jfgarcia0911/jfportfolio-2",
+		},
+		{
+			image: jobseeker,
+			title: "Jobseeker",
+			description:
+				"JobSeeker is a job-hunting platform that helps users search, apply, and manage job opportunities.",
+			link: "https://github.com/jfgarcia0911/jobseeker",
+			code: "https://github.com/jfgarcia0911/jobseeker",
+		},
+		{
+			image: authDemo,
+			title: "Firebase Authentication Demo",
+			description:
+				"A modern Next.js application that demonstrates seamless Firebase authentication integration with a clean, responsive UI. This demo showcases user management features including secure login/logout flows, profile display, and dynamic avatar handling.",
+			link: "https://firebase-auth-demo-theta.vercel.app/",
+			code: "https://github.com/jfgarcia0911/firebase-auth-demo",
+		},
 
-    const listProjects = [
-        {
-            image: amazon,
-            title: 'Amazon Clone App',
-            description: 'A full-stack e-commerce web application inspired by Amazon. Features include product listings, category-based search, detailed product pages, authentication, cart management, and order tracking. Built with React, Next.js, Firebase, and Tailwind CSS, the app delivers a responsive and user-friendly shopping experience.',
-            link: 'https://github.com/jfgarcia0911/amazon-clone'
-        },
-        {
-            image: portfolio,
-            title: 'Web Portfolio 2023',
-            description: 'A personal portfolio website built with pure HTML and CSS. It showcases your projects, skills, and experience with a clean and responsive design.',
-            link: 'https://jfgarcia0911.github.io/webportfolio2023/'
-        },
-        {
-            image: portfolio2,
-            title: 'Web Portfolio 2025',
-            description: 'A modern, responsive portfolio website built with React 19 and Vite, featuring sleek animations and optimized performance. This portfolio showcases my work and skills with a clean, professional design.',
-            link: 'https://jfgarcia0911.github.io/jfportfolio-2/'
-        },{
-            image: authDemo,
-            title: 'Firebase Authentication Demo',
-            description: 'A modern Next.js application that demonstrates seamless Firebase authentication integration with a clean, responsive UI. This demo showcases user management features including secure login/logout flows, profile display, and dynamic avatar handling.',
-            link: 'https://github.com/jfgarcia0911/firebase-auth-demo'
-        },
-        {
-            image: portfolio,
-            title: 'FB Clone',
-            description: 'A full-stack social media application inspired by Facebook, built with modern web technologies to replicate core social networking features including story sharing, image posts, and real-time interactions.',
-            link: 'https://github.com/jfgarcia0911/fb-clone'
-        },
-        {
-            image: portfolio2,
-            title: 'Project Three',
-            description: 'This is a brief description of Project Three.',
-            link: 'https://example.com/project3'
-        }
-    ];
-  return (
-    <main id='projects' className='p-4 '>
-        <section  data-aos='fade-up' data-aos-delay='300'>
-            <header className='text-center'>
-                <h1 className='text-3xl text-white sm:text-4xl font-bold mb-6'>
-                    My <span className='text-blue-400'>Projects</span>
-                </h1>
-                <p className='text-gray-400 mt-2 sm:mt-4 text-sm sm:text-base'>
-                    Here are some of the projects I’ve built, showcasing my skills in frontend development, responsive design, and modern web technologies.
-
-                </p>
-            </header>
-        </section>
-        <section data-aos='fade-up' data-aos-delay='500' className='flex flex-wrap gap-4 justify-center mt-6 '>
-            {listProjects.map((project,index) =>
-           (
-                <ProjectCard 
-                    key={index} 
-                    value={index}
-                    image={project.image} 
-                    title={project.title} 
-                    description={project.description} 
-                    link={project.link} 
-                />
-            ))}
-        </section>
-
-    </main>
-  )
+		{
+			image: portfolio,
+			title: "Web Portfolio 2023",
+			description:
+				"A personal portfolio website built with pure HTML and CSS. It showcases your projects, skills, and experience with a clean and responsive design.",
+			link: "https://jfgarcia0911.github.io/webportfolio2023/",
+			code: "https://github.com/jfgarcia0911/webportfolio2023",
+		},
+	];
+	return (
+		<main id="projects" className="p-4 ">
+			<section data-aos="fade-up" data-aos-delay="300">
+				<header className="text-center">
+					<h1 className="text-3xl text-white sm:text-4xl font-bold mb-6">
+						My <span className="text-blue-400">Projects</span>
+					</h1>
+					<p className="text-gray-400 mt-2 sm:mt-4 text-sm sm:text-base">
+						Here are some of the projects I’ve built, showcasing my
+						skills in frontend development, responsive design, and
+						modern web technologies.
+					</p>
+				</header>
+			</section>
+			<section
+				data-aos="fade-up"
+				data-aos-delay="500"
+				className="flex flex-wrap gap-4 justify-center mt-6 "
+			>
+				{listProjects.map((project, index) => (
+					<ProjectCard
+						key={index}
+						code={project.code}
+						image={project.image}
+						title={project.title}
+						description={project.description}
+						link={project.link}
+					/>
+				))}
+			</section>
+		</main>
+	);
 }
