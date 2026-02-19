315a4c5ee69fec0275f4fc44db11644ea96b8601
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-02-19 19:28:13 +0800

    feat(ProjectCard): conditionally hide Live Preview/View Code buttons if link and code match

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 export default function ProjectCard({ image, title, description, link, code }) {
 	const [expanded, setExpanded] = useState(false);
 
+	// Check if link and code are the same
+	const frontendCode = link === "https://github.com/jfgarcia0911/devconnectorFrontend";
+	const backendCode = code === "https://github.com/jfgarcia0911/devconnectorBackend";
+	const hideButton = link && code && link === code;
+
 	return (
 		<article className="relative max-w-sm bg-gray-800 rounded overflow-hidden shadow-lg group flex flex-col self-start w-100">
 			<div className="absolute z-0 w-40 h-40 sm:w-60 sm:h-60 bg-[#483cf5] rounded-full blur-3xl opacity-50 -top-5 left-10"></div>
@@ -15,22 +20,23 @@ export default function ProjectCard({ image, title, description, link, code }) {
 						className="w-full h-48 object-contain transition-transform duration-300 group-hover:scale-110"
 					/>
 					<div className="absolute gap-3 inset-0 h-[202px] flex items-center justify-center bg-blue-800 bg-opacity-50 opacity-0 group-hover:opacity-80 transition-opacity duration-300">
-						<a
+						{!hideButton && (<a
 							href={link}
 							target="_blank"
 							rel="noopener noreferrer"
 						>
 							<button className="bg-white font-medium text-black py-2 px-4 rounded-3xl shadow hover:text-white hover:bg-[#2879d5] cursor-pointer">
-								Live Preview
+								{frontendCode ? "Frontend Code" : "Live Preview"}
 							</button>
-						</a>
+						</a>)}
+						
 						<a
 							href={code}
 							target="_blank"
 							rel="noopener noreferrer"
 						>
 							<button className="bg-white font-medium text-black py-2 px-4 rounded-3xl shadow hover:text-white hover:bg-[#2879d5] cursor-pointer">
-								View Code
+								{backendCode ? "Backend Code" : "View Code"}
 							</button>
 						</a>
 					</div>
